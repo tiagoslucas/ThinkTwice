a59fcd08a45e666153cd440b7df8bd29fe2dadf7
Fábio Henriques <fabiomchenriques@ua.pt>
2020-03-06 23:33:16 +0000

    4: v2

--- challenge4/main.py
+++ challenge4/main.py
@@ -11,28 +11,29 @@ def isPrime(number):
 with open('input2.txt') as f:
     lines = [line.rstrip() for line in f]
     n, c = lines[0].split(" ")
-    n = (int)(n)
-    c = (int)(c)
-    out = []
-    for i in range(1, n + 1):
-        if isPrime(i):
-            out.append(i)
 
-    result = []
+n = (int)(n)
+c = (int)(c)
+out = []
+for i in range(1, n + 1):
+    if isPrime(i):
+        out.append(i)
 
-    if len(out) > 2 * c:
-        if len(out) % 2 == 0:
-            for i in range(0, c):
-                result.append(out[math.floor(len(out) / 2 - 1) + (i + 1)])
-                result.append(out[math.floor(len(out) / 2) - (i + 1)])
-        else:
-            result.append(out[2 * c])
-            for i in range(0, c-1):
-                result.append(out[math.floor(len(out) / 2) - (i + 1)])
-                result.append(out[math.floor(len(out) / 2) + (i + 1)])
+result = []
+
+if len(out) > 2 * c:
+    if len(out) % 2 == 0:
+        for i in range(0, c):
+            result.append(out[math.floor(len(out) / 2 - 1) + (i + 1)])
+            result.append(out[math.floor(len(out) / 2) - (i + 1)])
     else:
-        result = out
-    f = open("result.txt", "w")
-    f.write(str(n) + " " + str(c) + ": " +
-            str(sorted(result))[1:-1].replace(",", ""))
-    f.close()
+        result.append(out[2 * c])
+        for i in range(0, c-1):
+            result.append(out[math.floor(len(out) / 2) - (i + 1)])
+            result.append(out[math.floor(len(out) / 2) + (i + 1)])
+else:
+    result = out
+f = open("result.txt", "w")
+f.write(str(n) + " " + str(c) + ": " +
+        str(sorted(result))[1:-1].replace(",", ""))
+f.close()
