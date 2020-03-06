4120d0a1dc7d28a14fc194394849a75d383e748e
tiagoslucas <tiago.slucas@gmail.com>
2020-03-06 23:20:32 +0000

    1: v1

---
+++ challenge1/main.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char** argv) {
+	int ordem = 5, valor = 5;
+	FILE* input;
+	FILE* output;
+	input = fopen(argv[1], "r");
+	if (fscanf(input, " %d %d", &ordem, &valor) != 2) {
+		exit(EXIT_FAILURE);
+	}
+	fclose(input);
+
+	output = fopen("result.txt", "w");
+	fprintf(output, "%d", (int) pow((double)valor, 1/ (double)ordem));
+	fclose(output);
+
+	return 0;
+}
