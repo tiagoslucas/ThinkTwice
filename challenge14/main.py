a6e7a3ae8f1cace8e0123bde2f9e84ea6f5c3ffd
Fábio Henriques <fabiomchenriques@ua.pt>
2020-03-07 04:56:31 +0000

    14: v1

--- challenge14/main.py
+++ challenge14/main.py
@@ -1,7 +1,31 @@
-with open('input1.txt') as f:
+import sys
+with open(sys.argv[1]) as f:
     lines = [line.rstrip() for line in f]
-    colors = lines[0].split(" ")
-
-matrix = []
+    init_colors = lines[0].split(" ")
+dic = {}
+colors = {}
 for i in range(1, len(lines)):
-    matrix.append(lines[i].split(" "))
+    j = lines[i].split(" ")
+    dic[j[0]] = []
+    dic[j[1]] = []
+
+for i in range(1, len(lines)):
+    j = lines[i].split(" ")
+    dic[j[0]].append(j[1])
+    dic[j[1]].append(j[0])
+    colors[j[0]] = -1
+    colors[j[1]] = -1
+
+for l in colors:
+    used = []
+    for k in dic[l]:
+        used.append(colors[k])
+    for i in range(0, len(init_colors)):
+        if not i in used:
+            colors[l] = i
+            break
+
+f = open("result.txt", "w")
+for color in colors:
+    f.write(color + " " + init_colors[colors[color]] + "\n")
+f.close()
