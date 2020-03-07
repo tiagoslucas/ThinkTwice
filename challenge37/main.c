60423165bc53cd92d43fd14611db4630ef96981a
tiagoslucas <tiago.slucas@gmail.com>
2020-03-07 21:32:39 +0000

    37: v2

--- challenge37/main.c
+++ challenge37/main.c
@@ -3,21 +3,35 @@
 #include <math.h>
 
 int main(int argc, char** argv) {
-	int n, result = 0;
-	int vetori[10000] = { 0 };
-	int vetorf[10000] = { 0 };
+	int n, lim, result = 0;
+	int vetori[50] = { 0 };
+	int vetorf[50] = { 0 };
 	FILE* input;
 	FILE* output;
 	input = fopen(argv[1], "r");
 	if (fscanf(input, " %d", &n) != 1) {
 		exit(EXIT_FAILURE);
 	}
-	for (int i = 0; i < (n * n * (n − 1) / 2); i++){
-		fscanf(input, " %d", vetori[i]);
+	lim = n * n * (n - 1) / 2;
+	for (int i = 0; i < lim; i++){
+		fscanf(input, " %d", &vetori[i]);
 	}
 	fclose(input);
 
-	
+	for (int i = 0; i < n; i++) {
+		for (int j = i - 1; j > 0; j--) {
+			if ( vetori[i] <= vetori[j] || ((vetori[i] - vetori[j]) + (vetori[j] - vetori[0])) != (vetori[i] - vetori[0]) ) {
+				result = 2;
+				break;
+			}
+		}
+		if (result == 0) {
+			if (i < 1)
+				vetorf[i] = 0;
+			else
+				vetorf[i] = vetori[i] - vetorf[0];
+		}
+	}
 	
 	output = fopen("result.txt", "w");
 	if (result == 0) {
