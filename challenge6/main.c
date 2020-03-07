34c69424d00aec7cef3d864a250b76aad5bb274d
tiagoslucas <tiago.slucas@gmail.com>
2020-03-07 01:44:33 +0000

    6: v1

---
+++ challenge6/main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char** argv) {
+	double result;
+	int k, m;
+	FILE* input;
+	FILE* output;
+
+	input = fopen(argv[1], "r");
+	if (fscanf(input, " %d %d", &k, &m) != 2) {
+		exit(EXIT_FAILURE);
+	}
+	fclose(input);
+
+	result = (k + 1);
+	result = (m + k) / result;
+	result = m * result;
+
+	output = fopen("result.txt", "w");
+	fprintf(output, "%d", (int) ceil(result));
+	fclose(output);
+
+	return 0;
+}
