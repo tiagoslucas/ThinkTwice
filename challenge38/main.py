c8b2b8f5e68e20f42beea99014888b237bc18c64
Fábio Henriques <fabiomchenriques@ua.pt>
2020-03-07 15:40:52 +0000

    38: v2

--- challenge38/main.py
+++ challenge38/main.py
@@ -27,6 +27,9 @@ else:
     fibber = fiblike([1] + [2**i for i in range(k-1)])
     seq = [fibber(i) for i in range(100)]
 
+fibber = fiblike([1] + [2**i for i in range(1)])
+fib = [fibber(i) for i in range(100)]
+
 
 def nearestSmallerEqFib(n, seq):
     for i in range(1, len(seq)):
@@ -37,19 +40,40 @@ def nearestSmallerEqFib(n, seq):
 def printFibRepresntation(n, seq):
     arr = []
     while n > 0:
-        for i in range(0, len(seq)):
+        for i in range(1, len(seq)):
             if seq[i] > n:
                 n = n - seq[i - 1]
                 arr.append(seq[i - 1])
                 break
-    return arr[-1]
+    return arr
 
 
+# Esta a usar a n-sequencia
 if n in seq[2:]:
     f = open("result.txt", "w")
     f.write("lose")
     f.close()
 else:
+    var = -1
+    if k == 1:
+        if n % 2 != 0:
+            var = 1
+    else:
+        for i in range(1, n):
+            if n - i in seq[2:]:
+                if i * k >= n - i:
+                    isValid = False
+                    for l in list(reversed(range(1, i))):
+                        if l * k < n - l:
+                            var = l
+                            isValid = True
+                            break
+                    if isValid:
+                        break
+                else:
+                    var = i
+                    break
+
     f = open("result.txt", "w")
-    f.write(str(printFibRepresntation(n, seq)))
+    f.write(str(var))
     f.close()
