0fadf886288019af64b42b41a67add327974abf4
tiagoslucas <tiago.slucas@gmail.com>
2020-03-08 11:58:30 +0000

    Shortening range

--- challenge27/main.py
+++ challenge27/main.py
@@ -2,13 +2,13 @@ import sys
 
 with open(sys.argv[1], 'r') as input:
     n = int(input.read())
-for i in range(10 ** 15):
+for i in range(10 ** 5):
 	list=[]
 	for num in range(1,i+1):
 		if i%num == 0:
 			list.append(num)
 	if len(list) == n:
-		file = open('team15_ttwins/challenge27/result.txt','w')
+		file = open('result.txt','w')
 		file.write(str(num))
 		file.close()
 		break
