b083e8fb5cb15dca2ee072ef4a9804ab51b05ca6
tiagoslucas <tiago.slucas@gmail.com>
2020-03-08 08:32:20 +0000

    Small change

--- challenge27/main.py
+++ challenge27/main.py
@@ -6,8 +6,8 @@ def main():
 			d = int(i.strip())
 			if (d < 10 ** 15 + 1):
 				for i in range(10 ** 15 + 1):
-					count = 1
-					for j in range(1, i):
+					count = 0
+					for j in range(1, i + 1):
 						if (i % j) == 0:
 							count += 1
 						if count == d:
