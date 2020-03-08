1802aa42acf7bc9bae852384d979ca06d17fc874
tiagoslucas <tiago.slucas@gmail.com>
2020-03-08 08:55:16 +0000

    Limiting inputs

--- challenge27/main.py
+++ challenge27/main.py
@@ -4,7 +4,7 @@ def main():
 	with open(sys.argv[1]) as input:
 		for i in input:
 			d = int(i.strip())
-			if (d < 10 ** 15 + 1):
+			if (d <= 5000 and d > 0):
 				for i in range(10 ** 15 + 1):
 					count = 0
 					for j in range(1, i + 1):
@@ -16,7 +16,7 @@ def main():
 							file.close()
 							return 0
 			file = open('team15_ttwins/challenge27/result.txt', 'w')
-			file.write('Impossible')
+			file.write('Impossible’')
 			file.close()
 
 if __name__ == '__main__':
