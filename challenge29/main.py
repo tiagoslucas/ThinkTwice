74a1703e45eee90fd94d26ac2ba47865eb980ed6
Cláudio Henriques <c.henriques@ua.pt>
2020-03-08 09:59:30 +0000

    29: v2

--- challenge29/main.py
+++ challenge29/main.py
@@ -1,25 +1,25 @@
-from itertools import product
-import sys 
-validTuple = []
-n = 0
-with open(sys.argv[1], 'r', encoding="utf8") as f:
-    n = int(f.read())
+# Iterative function 
+def lucas(n) : 
+  
+    # declaring base values 
+    # for positions 0 and 1 
+    a = 2
+    b = 1
+      
+    if (n == 0) : 
+        return a 
+   
+    # generating number 
+    for i in range(2, n + 1) : 
+        c = a + b 
+        a = b 
+        b = c 
+      
+    return b 
 
-for i in [p for p in product([0,1],repeat=n)]:
-    prev = i[0]
-    isValidTuple = True
-    for j in range(1,len(i)):
-        if i[j] == prev and prev == 1:
-            isValidTuple = False
-            break
-        if j == len(i)-1 and i[j] == i[0] and i[j] == 1:
-            isValidTuple = False
-            break
-        prev = i[j]
-    
-    if isValidTuple:
-        validTuple.append(i)
+with open(sys.argv[1], 'r') as input:
+    n = input.read()
 
-f = open("team15_ttwins/challenge29/result.txt", "w")
-f.write(len(validTuple))
+f = open('team15_ttwins/challenge29/result.txt', 'w', encoding='utf8')
+f.write(lucas(n))
 f.close()
