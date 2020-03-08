63b1586fff9b3def8fe7875c87484db64d837055
Fábio Henriques <fabiomchenriques@ua.pt>
2020-03-08 12:57:06 +0000

    15

--- challenge15/main.py
+++ challenge15/main.py
@@ -8,14 +8,13 @@ m = (int)(lines[0].split(' ')[1])
 step = 1
 count = 0
 while n < m:
+    if step + 1 < n and count != 0:
+        step += 1
     n += step
     if n - m > step:
         step = step + 1
     else:
         step = 1
-    print(step, n)
-    if step + 1 < n:
-        step += 1
     count += 1
 
 f = open("team15_ttwins/challenge15/result.txt", "w")
