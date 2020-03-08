016ecf94780e9049d9abd9e3daafff3819c0eab5
Cláudio Henriques <c.henriques@ua.pt>
2020-03-08 10:55:44 +0000

    Fix Tiagos' bug

--- challenge35/main.py
+++ challenge35/main.py
@@ -48,7 +48,7 @@ for perm in list:
 
 prob = 1-(notFav/len(list))
 
-f = open('result.txt', 'w')
+f = open('team15_ttwins/challenge35/result.txt', 'w')
 if prob > 0.5:
     f.write(f"Bet for. {prob:.4f}")
 elif prob < 0.5:
