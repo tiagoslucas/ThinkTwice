516c8bf545c031efb8c5b8afe7c56cc497828c40
tiagoslucas <tiago.slucas@gmail.com>
2020-03-08 07:54:03 +0000

    Added Impossible case

--- challenge27/main.py
+++ challenge27/main.py
@@ -5,15 +5,18 @@ def main():
 		for i in input:
 			d = int(i.strip())
 			for i in range(10 ** 15 + 1):
-				count = 0
+				count = 1
 				for j in range(1, i):
 					if (i % j) == 0:
 						count += 1
 					if count == d:
-						file = open('team15_ttwins/challenge35/result.txt', 'w')
-						file.write(str(d))
+						file = open('result.txt', 'w')
+						file.write(str(i))
 						file.close()
 						return 0
+			file = open('result.txt', 'w')
+			file.write('Impossible')
+			file.close()
 
 if __name__ == '__main__':
 	main()
