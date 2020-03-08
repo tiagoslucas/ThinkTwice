447791272f4ae0c1228a5f5dbf09e59b6653c6eb
Cláudio Henriques <c.henriques@ua.pt>
2020-03-08 10:41:20 +0000

    Clean output

--- challenge27/main.py
+++ challenge27/main.py
@@ -3,19 +3,20 @@ import sys
 def main():
 	with open(sys.argv[1], 'r') as input:
 		for i in input:
-			d = int(i.strip()) if (d <= 5000 and d > 0)
-			for i in range(10 ** 15 + 1):
-				count = 0
-				for j in range(1, i + 1):
-					if (i % j) == 0:
-						count += 1
-					if count == d:
-						file = open('team15_ttwins/challenge27/result.txt', 'w')
-						file.write(str(i))
-						file.close()
-						return 0
+			d = int(i.strip())
+			if (d <= 5000 and d > 0):
+				for i in range(10 ** 15 + 1):
+					count = 0
+					for j in range(1, i + 1):
+						if (i % j) == 0:
+							count += 1
+						if count == d:
+							file = open('team15_ttwins/challenge27/result.txt', 'w')
+							file.write(str(i))
+							file.close()
+							return 0
 			file = open('team15_ttwins/challenge27/result.txt', 'w')
-			file.write('Impossible’')
+			file.write('Impossible')
 			file.close()
 
 if __name__ == '__main__':
