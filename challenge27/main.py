2b2ecdd822cc4c9183c5ef25e6d52059f94df1f4
tiagoslucas <tiago.slucas@gmail.com>
2020-03-08 08:11:40 +0000

    Adding impossible condition

--- challenge27/main.py
+++ challenge27/main.py
@@ -4,16 +4,17 @@ def main():
 	with open(sys.argv[1]) as input:
 		for i in input:
 			d = int(i.strip())
-			for i in range(10 ** 15 + 1):
-				count = 1
-				for j in range(1, i):
-					if (i % j) == 0:
-						count += 1
-					if count == d:
-						file = open('result.txt', 'w')
-						file.write(str(i))
-						file.close()
-						return 0
+			if (d < 10 ** 15 + 1):
+				for i in range(10 ** 15 + 1):
+					count = 1
+					for j in range(1, i):
+						if (i % j) == 0:
+							count += 1
+						if count == d:
+							file = open('result.txt', 'w')
+							file.write(str(i))
+							file.close()
+							return 0
 			file = open('result.txt', 'w')
 			file.write('Impossible')
 			file.close()
